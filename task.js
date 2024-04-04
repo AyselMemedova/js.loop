ff52795079f2ce3381f88d5894e360358ad503a2
AyselMemedova <memmdovaysel@gmail.com>
2024-04-04 10:21:19 +0400

    first commit

---
+++ task.js
@@ -0,0 +1,40 @@
+//Verilmis array-deki reqemlerinin cemi 10-dan kicik olan ededlerin sayini tapan alqoritm
+
+// let array = [9, 12, 0, 8, 23,56,];
+// let summ = 0;
+//  for(let i = 0; i<array.length; i++){
+//     if(array[i]<10){
+//       summ ++
+//      }
+// }
+// console.log(summ);
+
+
+
+//verilmish ededin 3-un quvveti olub, olmadigini tapan alqoritm yazin.
+// let eded = parseInt(prompt("Ədəd daxil edin:"));
+
+// while (eded % 3 == 0) {
+//     eded /= 3;
+// }
+
+// if (eded == 1) {
+//     console.log("Ədəd 3-ün quvvətidir.");
+// } else {
+//     console.log("Ədəd 3-ün quvvəti deyil.");
+// }
+
+
+//Verilmis ededin en yaxin asagi kokalti deyerini tapan alqoritm
+
+// let eded =parseInt(prompt("Ədəd daxil edin:"));
+// let yaxinKok=0;
+// for(let i = 0; i**2 <= eded; i++){
+//     yaxinKok=i;
+//     if (yaxinKok **2 > eded) {
+//         yaxinKok --;
+        
+//     }
+// }
+
+// console.log(yaxinKok);
